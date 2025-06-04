cca39d3e8966d0cfe36b3320c294a1e1c3c285c6
ShubhamKumar37 <sk9818281820@gmail.com>
2025-06-04 10:55:16 +0530

    learning jquery and started with some animations

---
+++ base/js/script.js
@@ -0,0 +1,29 @@
+$(function()
+{
+  // $(".red-box ").fadeOut(1000).fadeIn(1000);
+  // $(".green-box ").fadeOut(1000);
+  // $(".red-box ").fadeTo(1000, 0.3);
+
+  // $(".red-box").fadeOut(1000);
+  // We can use normal css selector for this
+  // Duplicacy will keep the answer same 
+
+  // $(".red-box").fadeOut(1000);
+  // $(".green-box").fadeOut(1000);
+  // $(".blue-box").fadeOut(1000);
+  // $(".red-box").fadeTo(1000, 0.2);
+  // $(".green-box").fadeTo(1000, 0.5);
+  // $(".blue-box").fadeTo(1000, 0.8);
+
+  // $(".red-box").fadeToggle().fadeToggle();
+  // $(".green-box").fadeToggle().fadeToggle();
+  // $(".blue-box").fadeToggle().fadeToggle();
+
+  // Fade will change the display property and will not able to change it again if have dispaly none property.
+
+  // $(".red-box").fadeOut(1000);
+  // $(".red-box").fadeTo(1000, 0.3);
+  // $(".blue-box").hide();
+  // $(".blue-box").toggle();
+
+}); 
